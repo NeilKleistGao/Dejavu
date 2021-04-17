c71d1530a52dc4fb49bf4cd7797e0a78bbd1009c
asja-aaa <1980490930@qq.com>
2021-04-17 23:15:50 +0800

    add load balancing

--- backend/personal-manage-model/src/main/java/com/buptse/config/ConfigBean.java
+++ backend/personal-manage-model/src/main/java/com/buptse/config/ConfigBean.java
@@ -2,20 +2,19 @@ package com.buptse.config;
 
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-// @Configuration --- spring applicationContext.xml
+@LoadBalancerClient(name = "UTIL-SERVICE-PROVIDER",configuration = CustomLoadBalancerConfiguration.class)
 public class ConfigBean {
-
-    //配置负载均衡实现RestTemplate
-
-
+    //将这个对象放入ioc容器
     @Bean
-    @LoadBalanced //Ribbon
+    @LoadBalanced  //使用这个注解给restTemplate赋予了负载均衡的能力
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
+
 }
