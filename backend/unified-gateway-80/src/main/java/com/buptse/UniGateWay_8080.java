9bac401aca210591e7afdefd430daf99aaa7e3b3
asja-aaa <1980490930@qq.com>
2021-04-19 14:35:01 +0800

    add Website Architecture

--- backend/unified-gateway-80/src/main/java/com/buptse/UniGateWay_8080.java
+++ backend/unified-gateway-80/src/main/java/com/buptse/UniGateWay_8080.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UniGateWay_80 {
+public class UniGateWay_8080 {
     public static void main(String[] args) {
-        SpringApplication.run(UniGateWay_80.class,args);
+        SpringApplication.run(UniGateWay_8080.class,args);
     }
 }
