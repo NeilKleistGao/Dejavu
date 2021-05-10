571d1a108ebce9a71fd3b56a39ba38954a2210e9
zoeisgreat <zoebornslippy@163.com>
2021-05-10 14:20:48 +0800

    Add BuyBox to Index.vue

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -33,14 +33,14 @@ module.exports = function (/* ctx */) {
     extras: [
       // 'ionicons-v4',
       // 'mdi-v5',
-      // 'fontawesome-v5',
       // 'eva-icons',
       // 'themify',
       // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       'roboto-font', // optional, you are not bound to it
-      'material-icons' // optional, you are not bound to it
+      'material-icons', // optional, you are not bound to it
+      'fontawesome-v5'
     ],
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -21,7 +21,74 @@
     </q-form>
     <br/>
     <div class="parentBuySell">
-      <div class="child" style="width:700px"></div>
+      <div class="child" style="width:700px">
+        <div>
+          <q-btn color="blue" icon="fas fa-car" icon-right="send" label="我要买车" v-on:click="gotoSearch"/>
+        </div>
+        <div class="parent" style="height: 20px"></div>
+        <div>
+          <q-btn-toggle
+            v-on:click="gotoSearch"
+            v-model="message"
+            spread
+            toggle-color="primary"
+            :options="[
+            {label: '大众', value: '大众'},
+            {label: '宝马', value: '宝马'},
+            {label: '奔驰', value: '奔驰'},
+            {label: '丰田', value: '丰田'},
+            {label: '本田', value: '本田'},
+            {label: '奥迪', value: '奥迪'},
+            {label: '别克', value: '别克'},
+          ]"
+          />
+          <q-btn-toggle
+            v-on:click="gotoSearch"
+            v-model="message"
+            spread
+            toggle-color="primary"
+            :options="[
+              {label: '日产', value: '日产'},
+              {label: '福特', value: '福特'},
+              {label: '现代', value: '现代'},
+              {label: '马自达', value: '马自达'},
+              {label: '雪佛兰', value: '雪佛兰'},
+              {label: '吉利', value: '吉利'},
+              {label: 'Jeep', value: 'Jeep'},
+            ]"
+          />
+          <q-btn-toggle
+            v-on:click="gotoSearch"
+            v-model="message"
+            spread
+            toggle-color="primary"
+            :options="[
+            {label: '3万以下', value: '3'},
+            {label: '3-5万', value: '3-5'},
+            {label: '5-7万', value: '5-7'},
+            {label: '7-9万', value: '7-9'},
+            {label: '9-12万', value: '9-12'},
+            {label: '12-16万', value: '12-16'},
+            {label: '16-20万', value: '16-20'},
+          ]"
+          />
+          <q-btn-toggle
+            v-on:click="gotoSearch"
+            v-model="message"
+            spread
+            toggle-color="primary"
+            :options="[
+            {label: '超值', value: '3'},
+            {label: '急售', value: '3-5'},
+            {label: '练手车', value: '5-7'},
+            {label: '准新车', value: '7-9'},
+            {label: '严选车', value: '9-12'},
+            {label: 'SUV', value: '12-16'},
+            {label: '查看更多', value: ''},
+          ]"
+          />
+        </div>
+      </div>
       <div class="child" style="width:500px; height:280px;"></div>
     </div>
     <br/>
