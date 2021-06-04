be38613de253da181a085b17bf725279f2d23b59
asja-aaa <1980490930@qq.com>
2021-06-04 18:54:49 +0800

    add user_query_all api

--- backend/util-service-provider-8001/src/main/java/com/buptse/controller/UserController.java
+++ backend/util-service-provider-8001/src/main/java/com/buptse/controller/UserController.java
@@ -3,6 +3,7 @@ package com.buptse.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.buptse.common.util.*;
+import com.buptse.dto.CommonResult;
 import com.buptse.mapper.UserMapper;
 import com.buptse.pojo.Car;
 import com.buptse.pojo.User;
@@ -26,18 +27,27 @@ public class UserController {
 
     @Autowired
     private IUserService service;
-    @Autowired
-    private ICarService carService;
+
     @GetMapping("/user/get/{uid}")
     public User getUSerById(@PathVariable("uid") Integer uid) {
         User user = service.getById(uid);
         return service.getById(uid);
     }
-    @GetMapping("/car/{id}")
-    public Car getCar(@PathVariable("id") Integer carId){
-        Car car = carService.getById(carId);
-        return car;
+
+    @GetMapping("/user/info/query_all")
+    public CommonResult queryAllUser(
+            @RequestParam(value = "name",required = false) String name,
+            @RequestParam(value = "phone",required = false) String phone,
+            @RequestParam(value = "email",required = false) String email){
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        if(name!=null){userQueryWrapper.eq("name",name);}
+        if(phone!=null){userQueryWrapper.eq("phone_number",phone);}
+        if(email!=null){userQueryWrapper.eq("mail",email);}
+        List<User> userList = service.list(userQueryWrapper);
+        return CommonResult.success(userList);
+
     }
+
 
 
 
