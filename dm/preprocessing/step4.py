0ace773afe06600462c162dca9f7b71cd294927f
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2021-09-06 10:01:19 +0800

    add model storage

--- dm/preprocessing/step4.py
+++ dm/preprocessing/step4.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+# 删除异常值
 if __name__ == '__main__':
     df = pd.read_csv("../dataset/temp3.csv")
+    df.drop(df[df.power > 600].index, inplace=True)
+    print(df.head(3))
     df.to_csv("../dataset/temp4.csv", index=False)
